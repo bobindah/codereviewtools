6d28ab2b530e98e81318ea5d234f11390a62126e
Robert Triggs <rstriggs@gmail.com>
2018-01-23 19:29:18 +0000

    Add project files.

---
+++ CodeReviewPairs/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodeReviewPairs.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
